750e40475764cf663ad537ed00360242d21e813e
Pedro Henrique <zpedrogod59@gmail.com>
2025-04-21 22:46:14 -0300

    feat: implement user details page and related functionalities

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GithubProvider } from './entities/github-provider.entity';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, GithubProvider])],
   exports: [UsersService],
   providers: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
